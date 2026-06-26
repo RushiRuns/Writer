f337d39f818825df0ecb8578f755f1f4a5d8dc69
RushiRuns <rushikeshaundhakar715@gmail.com>
2026-06-26 19:57:16 +0530

    chore(main): remove default window menu bar for frameless clean layout

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,8 @@ const createWindow = () => {
     },
   });
 
+  mainWindow.setMenu(null);
+
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
